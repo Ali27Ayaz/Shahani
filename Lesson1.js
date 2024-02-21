0d38d2bedb342f51c72c346587fe9f90a2f27f2f
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-02-21 19:38:46 +0500

    Add files via upload

---
+++ Lesson1.js
@@ -0,0 +1,8 @@
+// Asslam O Alaikum
+console.log("Hellow World");
+console.log();
+console.log("or");
+console.log();
+console.log("We can call it A.I. World, World is changing");
+console.log();
+console.log("Its my first Class of Typescript & I am enjoying it");
